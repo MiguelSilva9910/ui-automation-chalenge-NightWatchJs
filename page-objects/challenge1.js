73b125a71a9d12a47141cb954531262880d65c66
MiguelSilva <miguel.silva@school.mindera.com>
2019-07-01 14:48:28 +0100

    Challenge1 with page objects

---
+++ page-objects/challenge1.js
@@ -0,0 +1,10 @@
+module.exports = {
+    url: 'https://mindera.com',
+    elements: {
+         menu: "[data-reactid='17']",
+         peopleAndCulture: "[data-reactid='27']" ,
+         locals: "[data-reactid='100']",
+         location: "#contacts > div > div.footer-contacts > div:nth-child(1) > div:nth-child(2) > div.footer-contact-city",
+         jobs: "a.jobs-item:last-of-type"
+    }
+};
