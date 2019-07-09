2af857f3047d37f45b4b7d4d4dc20093a4889f4c
MiguelSilva <miguel.silva@school.mindera.com>
2019-07-09 11:13:41 +0100

    Refactoring

--- tests/challenge3/viewInMobile.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'Challenge 3: Check if user can see in mobile'(browser) {
-        const page = browser.page.challenge3();
-
-        page
-            .navigate()
-            .expect.element('@iName').to.be.visible;
-    }
-}
